fbec0cf36fd7fd16b3c7b51b404bf18a58abf8de
David Morand <david.morand@nuecho.com>
2017-12-22 15:13:36 -0500

    first integration with server, game is created

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import Game from './polgo/Game.js';
 import './index.css';
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+const POST = {
+  method: 'POST'
+};
+
+fetch("./game/create", POST)
+.then(response => response.json())
+.then(renderGame)
+
+function renderGame(game) {
+  ReactDOM.render(
+    <Game gameId={game.uuid} board={game.board} />,
+    document.getElementById('root')
+  );
+}
--- src/polgo/Game.js
+++ src/polgo/Game.js
@@ -13,10 +13,6 @@ export default class Game extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
 
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -25,13 +21,7 @@ export default class Game extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div className="game">
@@ -39,6 +29,7 @@ export default class Game extends React.Component {
           <Board
             onClick={(i) => this.handleClick(i)}
             squares={this.state.squares}
+            board={this.props.board}
           />
         </div>
         <div className="game-info">
@@ -48,24 +39,4 @@ export default class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
