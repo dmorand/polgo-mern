6c148988c8cf5f4d0816438903b9a34ebd903296
dmorand <david.morand@gmail.com>
2018-01-05 10:38:07 -0500

    improved visuals

--- src/polgo/Square.js
+++ src/polgo/Square.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
+const WHITE = 'W';
+const BLACK = 'B';
+const ILLEGAL = 'I';
+
 export default class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
+      <button className={this.getClassName()} onClick={() => this.props.onClick()}>
+        {this.getContent()}
       </button>
     );
+  }
+
+  getClassName() {
+    if (this.props.value === ILLEGAL) return "square illegal";
+    return "square";
+  }
+
+  getContent() {
+    switch (this.props.value) {
+      case BLACK: return <div className="circle black"/>
+      case WHITE: return <div className="circle white"/>
+    }
   }
 }
